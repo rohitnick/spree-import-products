b32fb02bc8d0ca788b1421d9409a43f2443109a2
thearmchair <rohit@thearmchair.in>
2017-08-01 18:10:18 +0530

    updating data_file path

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -10,7 +10,7 @@ module Spree
 
   class ProductImport < ActiveRecord::Base
     #attr_accessible :data_file, :data_file_file_name, :data_file_content_type, :data_file_file_size, :data_file_updated_at, :product_ids, :state, :failed_at, :completed_at
-    has_attached_file :data_file, path: "public/spree/product_imports/data-files/:basename_:timestamp.:extension"
+    has_attached_file :data_file, path: ":rails_root/public/spree/product_imports/data-files/:basename_:timestamp.:extension"
     validates_attachment :data_file, presence: true, content_type: { content_type: ["text/csv", "text/plain", "text/comma-separated-values", "application/octet-stream", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"] }
 
 
